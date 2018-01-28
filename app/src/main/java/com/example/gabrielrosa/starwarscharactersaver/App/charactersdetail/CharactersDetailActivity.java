398f875eeef6a1220d951e2ad3fd54262067a482
Gaberu <gabrielrosadn@gmail.com>
2018-01-28 11:31:27 -0300

    initial commit
    Signed-off-by: Gaberu <gabrielrosadn@gmail.com>

---
+++ app/src/main/java/com/example/gabrielrosa/starwarscharactersaver/App/charactersdetail/CharactersDetailActivity.java
@@ -0,0 +1,19 @@
+package com.example.gabrielrosa.starwarscharactersaver.App.charactersdetail;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.gabrielrosa.starwarscharactersaver.R;
+
+/**
+ * Created by gabrielrosa on 28/01/18.
+ */
+
+public class CharactersDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen_act);
+    }
+}
---
+++ app/src/main/java/com/example/gabrielrosa/starwarscharactersaver/util/FragmentUtil.java
@@ -0,0 +1,24 @@
+package com.example.gabrielrosa.starwarscharactersaver.util;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Created by gabrielrosa on 28/01/18.
+ */
+
+public class FragmentUtil {
+
+    public static void replaceFragment(@NonNull FragmentManager fragmentManager,
+                                       @NonNull Fragment fragment, int frameId) {
+        checkNotNull(fragmentManager);
+        checkNotNull(fragment);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(frameId, fragment);
+        transaction.commit();
+    }
+}
